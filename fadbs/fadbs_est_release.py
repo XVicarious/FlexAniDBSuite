b402ebd23cad09d3fce18e2f9bc458bf77fdaf79
Brian Maurer <bmaurer@xvicario.us>
2018-10-15 20:20:34 -0400

    Start estimate_release plugin

---
+++ fadbs/fadbs_est_release.py
@@ -0,0 +1,28 @@
+from __future__ import unicode_literals, division, absolute_import
+from builtins import *  # noqa pylint: disable=unused-import, redefined-builtin
+
+import logging
+
+from flexget import plugin
+from flexget.event import event
+from flexget.utils.tools import split_title_year
+
+
+PLUGIN_ID = 'fadbs_est_release'
+
+log = logging.getLogger(PLUGIN_ID)
+
+
+class EstimateSeriesAniDb(object):
+    @plugin.priority(2)
+    def estimate(self, entry):
+        if not all(field in entry for field in ['series_name', 'series_season']):
+            log.debug('Failed first entry :(')
+            return
+        for k in entry:
+            log.debug('%s: %s', k, entry[k])
+
+
+@event('plugin.register')
+def register_plugin():
+    plugin.register(EstimateSeriesAniDb, PLUGIN_ID, interfaces=['estimate_release'], api_ver=2)
