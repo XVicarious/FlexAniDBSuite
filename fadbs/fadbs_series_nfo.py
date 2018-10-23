5d05960d6ca8de77c2f82b732b2afc7fb411faad
Brian Maurer <bmaurer@xvicario.us>
2018-10-22 21:51:41 -0400

    generate nfo files

--- fadbs/fadbs_series_nfo.py
+++ fadbs/fadbs_series_nfo.py
@@ -3,7 +3,9 @@ import os
 
 from flexget import plugin
 from flexget.event import event
-from jinja2 import Template
+from flexget.utils import template
+
+from .fadbs_lookup import FadbsLookup
 
 PLUGIN_ID = 'fadbs_series_nfo'
 
@@ -11,9 +13,10 @@ log = logging.getLogger(PLUGIN_ID)
 
 
 class FadbsSeriesNfo(object):
+
     plugin_path = os.path.realpath(__file__)
     last_sep = plugin_path.rfind(os.sep)
-    nfo_path = plugin_path[:last_sep] + os.sep + 'templates' + os.sep + 'tvshow.template.nfo'
+    nfo_path = plugin_path[:last_sep] + os.sep + 'templates' + os.sep + 'task' + os.sep + 'tvshow.nfo.template'
 
     schema = {
         'type': 'object',
@@ -28,9 +31,13 @@ class FadbsSeriesNfo(object):
             self.__generate_nfo(entry)
 
     def __generate_nfo(self, entry):
-        template = Template(self.nfo_path)
-
-        template.render(entry=entry)
+        filename = os.path.expanduser('tvshow.nfo.template')
+        # Load stuff
+        log.debug(FadbsLookup.schema)
+        for key in FadbsLookup.schema.keys():
+            log.debug(entry.get(key))
+        template_ = template.render_from_entry(template.get_template(filename), entry)
+        log.debug(template_.encode('utf-8'))
 
 
 @event('plugin.register')
