fd0f7d9bbce61d1e3327571f94ba93269e224d95
Brian Maurer <bmaurer@xvicario.us>
2018-10-24 19:12:00 -0400

    __generate_nfo moved to on_task_output add default definition of what should be considered a genre __genres to separate genres from tag increase default weight of a genre is

--- fadbs/fadbs_series_nfo.py
+++ fadbs/fadbs_series_nfo.py
@@ -5,8 +5,6 @@ from flexget import plugin
 from flexget.event import event
 from flexget.utils import template
 
-from .fadbs_lookup import FadbsLookup
-
 PLUGIN_ID = 'fadbs_series_nfo'
 
 log = logging.getLogger(PLUGIN_ID)
@@ -21,23 +19,58 @@ class FadbsSeriesNfo(object):
     schema = {
         'type': 'object',
         'properties': {
-            'genre_weight': {'type': 'integer', 'default': 300},
+            'genre_weight': {'type': 'integer', 'default': 400},
             'spoilers': {'type': 'array', 'items': {'type': 'string', 'enum': ['local', 'global']}}
         }
     }
 
-    def on_task_output(self, task, config):
-        for entry in task.entries:
-            self.__generate_nfo(entry)
+    # These are all genres, genres that are True don't have possible overriding sub-genres
+    # Genres that are False have possible overriding sub-genres
+    # genres with another genre id replace that genre as a genre (if that makes sense)
+    # todo: Make this more flexible, what if someone wants a sub-sub-genre to be a genre?
+    default_genres = {
+        2841: True, 2282: True,  # Action and martial arts
+        2850: True,  # Adventure
+        2853: True, 2655: True,  # Comedy and parody
+        2856: True,  # Ecchi ;)
+        2851: True,  # Horror
+        2858: True,  # Romance
+        2849: False, 2648: 2849, 2649: 2849, 2647: 2849, 2094: True,  # Fantasy and friends, and magic
+        2846: True, 2638: True,  # Science Fiction and Mecha
+        2623: True,  # Super Power
+        2887: True,  # Tragedy
+        2614: True, 1846: True, 2616: True, 1802: True, 1077: True, 922: True,  # Target Audiences
+        2864: True, 2869: True,  # Daily Life and School Life
+        2881: True  # Sports
+    }
 
-    def __generate_nfo(self, entry):
+    def on_task_output(self, task, config):
         filename = os.path.expanduser('tvshow.nfo.template')
-        # Load stuff
-        log.debug(FadbsLookup.schema)
-        for key in FadbsLookup.schema.keys():
-            log.debug(entry.get(key))
-        template_ = template.render_from_entry(template.get_template(filename), entry)
-        log.debug(template_.encode('utf-8'))
+        for entry in task.entries:
+            # Load stuff
+            entry_tags = entry.get('anidb_tags')
+            if entry_tags is None:
+                return
+            entry['fadbs_nfo']['genres'], entry['fadbs_nfo']['tags'] = \
+                self.__genres(entry.get('anidb_tags').items(), config['genre_weight'])
+            template_ = template.render_from_entry(template.get_template(filename), entry)
+            with open(entry.get('anidb_name') + '.tvshow.nfo', 'wb') as nfo:
+                nfo.write(template_.encode('utf-8'))
+                nfo.close()
+
+    def __genres(self, anidb_tags, genre_weight):
+        genres = []
+        tags = []
+        for aid, info in anidb_tags:
+            log.trace('%s: %s, weight %s', aid, info[0], info[1])
+            if aid in self.default_genres or info[1] >= genre_weight:
+                genres.append(info[0])
+                # todo: remove an overridden genre
+                continue
+            tags.append(info[0])
+        log.info('Genres: %s', genres)
+        log.info('Tags: %s', tags)
+        return genres, tags
 
 
 @event('plugin.register')
