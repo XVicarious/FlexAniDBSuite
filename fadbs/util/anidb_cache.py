8e40c66378f6781937271062d9fe485aef6c85ec
Brian Maurer <bmaurer@xvicario.us>
2018-10-24 18:59:34 -0400

    Fix line length

--- fadbs/util/anidb_cache.py
+++ fadbs/util/anidb_cache.py
@@ -4,5 +4,5 @@ from flexget.utils.cache import cached_resource
 
 
 def get_anidb_cache(url, ):
-    return cached_resource(url, '/home/xvicarious/PyProjects/FlexAniDBSuite/', max_size=1024, directory='anidb_cache')[
-        0]
+    return cached_resource(url, '/home/xvicarious/PyProjects/FlexAniDBSuite/',
+                           max_size=1024, directory='anidb_cache')[0]
