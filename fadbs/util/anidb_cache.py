4cc0cb7930a7b5ddc438335ddc01d5d4071ab561
Brian Maurer <bmaurer@xvicario.us>
2018-10-26 15:06:28 -0400

    split up return_cached

--- fadbs/util/anidb_cache.py
+++ fadbs/util/anidb_cache.py
@@ -15,6 +15,19 @@ ANIDB_CACHE = '.anidb_cache'
 def cached_anidb(func):
     anidb_anime_string = 'anime: %s'
 
+    def __get_blake_name(anidb_cache_name):
+        blake = hashlib.new('blake2b')
+        blake.update(anidb_cache_name)
+        return blake.hexdigest()
+
+    def __get_soup(file_path):
+        if os.path.exists(file_path):
+            with open(file_path, 'r') as cached_file:
+                soup = get_soup(cached_file, parser='lxml')
+                cached_file.close()
+                return soup
+        return None
+
     def return_cached(*args, **kwargs):
         anidb_id = args[0].anidb_id
         if anidb_id:
@@ -22,28 +35,14 @@ def cached_anidb(func):
             anidb_cache_name = (anidb_anime_string % anidb_id).encode()
             if 'blake2b' in hashlib.algorithms_available:
                 log.trace('blake2b is here!')
-                blake = hashlib.new('blake2b')
-                blake.update(anidb_cache_name)
-                blakesum = blake.hexdigest()
-                cache_file = os.path.join(manager.config_base, ANIDB_CACHE, blakesum)
-                if os.path.exists(cache_file):
-                    log.debug('%s is in the cache', anidb_id)
-                    with open(cache_file, 'r') as cached_file:
-                        soup = get_soup(cached_file, parser='lxml')
-                        cached_file.close()
-                        kwargs.update(soup=soup)
+                cache_file = os.path.join(manager.config_base, ANIDB_CACHE, __get_blake_name(anidb_cache_name))
+                kwargs.update(soup=__get_soup(cache_file))
             if 'soup' not in kwargs or not kwargs['soup']:
-                md5sum = hashlib.md5(anidb_cache_name).hexdigest()
-                cache_file = os.path.join(manager.config_base, ANIDB_CACHE, md5sum)
+                cache_file = os.path.join(manager.config_base, ANIDB_CACHE, hashlib.md5(anidb_cache_name).hexdigest())
                 if os.path.exists(cache_file):
-                    with open(cache_file) as cached_file:
-                        soup = get_soup(cached_file, parser='lxml')
-                        cached_file.close()
-                        kwargs.update(soup=soup)
+                    kwargs.update(soup=__get_soup(cache_file))
                     if 'blake2b' in hashlib.algorithms_available:
-                        blake = hashlib.new('blake2b')
-                        blake.update(anidb_cache_name)
-                        blake_path = os.path.join(manager.config_base, ANIDB_CACHE, blake.hexdigest())
+                        blake_path = os.path.join(manager.config_base, ANIDB_CACHE, __get_blake_name(anidb_cache_name))
                         os.rename(cache_file, blake_path)
         func(*args, **kwargs)
 
